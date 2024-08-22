9333e7b39fa510f19206359e57fea87f7f12a355
Jonathan Sabiniano <jonsabiniano03@gmail.com>
2024-08-22 17:01:56 +0800

    refactor package name import
    refactor import org.guanzon.cas.model.inventory to import org.guanzon.cas.inventory.models for all inventory validator class

--- src/main/java/org/guanzon/cas/validators/inventory/Validator_Inventory_Sub_Unit.java
+++ src/main/java/org/guanzon/cas/validators/inventory/Validator_Inventory_Sub_Unit.java
@@ -1,7 +1,7 @@
 package org.guanzon.cas.validators.inventory;
 
 import org.guanzon.appdriver.base.GRider;
-import org.guanzon.cas.model.inventory.Model_Inventory_Sub_Unit;
+import org.guanzon.cas.inventory.models.Model_Inventory_Sub_Unit;
 import org.guanzon.cas.validators.ValidatorInterface;
 
 /**
