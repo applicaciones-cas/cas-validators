0591a759fcf3573be43ec8de8de9c3b41ebc68da
Jonathan Sabiniano <jonsabiniano03@gmail.com>
2024-09-17 11:40:06 +0800

    add validator for inventory request

---
+++ src/main/java/org/guanzon/cas/validators/inventory/Validator_Inv_Stock_Request_Detail.java
@@ -0,0 +1,77 @@
+package org.guanzon.cas.validators.inventory;
+
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.cas.inventory.models.Model_Inv_Stock_Request_Detail;
+import org.guanzon.cas.inventory.models.Model_Inventory;
+import org.guanzon.cas.validators.ValidatorInterface;
+
+/**
+ *
+ * @author Michael Cuison
+ */
+public class Validator_Inv_Stock_Request_Detail implements ValidatorInterface {
+    GRider poGRider;
+    String psMessage;
+    
+    Model_Inv_Stock_Request_Detail poEntity;
+    
+    public Validator_Inv_Stock_Request_Detail(Object foValue){
+        poEntity = (Model_Inv_Stock_Request_Detail) foValue;
+    }
+
+    @Override
+    public void setGRider(GRider foValue) {
+        poGRider = foValue;
+    }
+    @Override
+    public boolean isEntryOkay() {
+        
+        if (poEntity.getBarcode().isEmpty()){
+            psMessage = "Barcode is not set.";
+            return false;
+        } 
+        if (poEntity.getDescription().isEmpty()){
+            psMessage = "Description is not set.";
+            return false;
+        }
+//        
+//        if (poEntity.getBrandCode().isEmpty()){
+//            psMessage = "Brand is not set.";
+//            return false;
+//        }
+//        if (poEntity.getModelCode().isEmpty()){
+//            psMessage = "Model is not set.";
+//            return false;
+//        }
+//        
+//        if (poEntity.getColorCode().isEmpty()){
+//            psMessage = "Color is not set.";
+//            return false;
+//        }
+//        if (Double.parseDouble(poEntity.g().toString())<=0.0){
+//            psMessage = "Unit Price is not set.";
+//            return false;
+//        }
+        if (Double.parseDouble(poEntity.getQuantity().toString())<=0.0){
+            psMessage = "Quantity Request is not set.";
+            return false;
+        }
+//        if (poEntity.getUnitType().isEmpty()){
+//            psMessage = "Unit Type is not set.";
+//            return false;
+//        }
+//        if (poEntity.getShlfLife()==null){
+//            psMessage = "ShelfLife is not set.";
+//            return false;
+//        }
+        return true;
+    }
+    
+
+    @Override
+    public String getMessage() {
+        return psMessage;
+    }
+
+}
+
