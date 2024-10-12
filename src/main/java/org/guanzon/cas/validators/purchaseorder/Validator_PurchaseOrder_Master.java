ce1a36cd8e5de98fc56823b7fb2b880ce949b9ba
Aldristh456034 <trinidadaldrich328@gmail.com>
2024-10-12 13:42:00 +0800

    Created Validators for Purchase Order Detail and Master

---
+++ src/main/java/org/guanzon/cas/validators/purchaseorder/Validator_PurchaseOrder_Master.java
@@ -0,0 +1,52 @@
+package org.guanzon.cas.validators.purchaseorder;
+import java.util.ArrayList;
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.cas.purchasing.model.Model_PO_Master;
+
+
+import org.guanzon.cas.validators.ValidatorInterface;
+
+/**
+ *
+ * @author Michael Cuison
+ */
+public class Validator_PurchaseOrder_Master implements ValidatorInterface {
+    GRider poGRider;
+    String psMessage;
+    
+    Model_PO_Master poEntity;
+    
+    public Validator_PurchaseOrder_Master(Object foValue){
+        poEntity = (Model_PO_Master) foValue;
+    }
+    
+
+    @Override
+    public void setGRider(GRider foValue) {
+        poGRider = foValue;
+    }
+    @Override
+    public boolean isEntryOkay() {
+        if (poEntity.getTransactionNo().isEmpty()){
+            psMessage = "Transaction Number is not set.";
+            return false;
+        }
+        if (poEntity.getDestination().isEmpty()){
+            psMessage = "Destination is not set.";
+            return false;
+        }
+        if (poEntity.getCategoryCode().isEmpty()){
+            psMessage = "Category is not set.";
+            return false;
+        }
+        return true;
+    }
+    
+
+    @Override
+    public String getMessage() {
+        return psMessage;
+    }
+
+}
+
